0708760d9a35baa479effdfd9fe61b96ef54da4e
Patrick Lai <patricklai10123@gmail.com>
2019-05-20 21:51:19 -0700

    add custom led configs to ctrl

--- tmk_core/protocol/arm_atsam/usb/compiler.h
+++ tmk_core/protocol/arm_atsam/usb/compiler.h
@@ -134,13 +134,13 @@
  * heuristics and inline the function no matter how big it thinks it
  * becomes.
  */
-#if defined(__CC_ARM)
-#  define __always_inline             __forceinline
-#elif (defined __GNUC__)
-#  define __always_inline             __attribute__((__always_inline__))
-#elif (defined __ICCARM__)
-#  define __always_inline             _Pragma("inline=forced")
-#endif
+// #if defined(__CC_ARM)
+// #  define __always_inline             __forceinline
+// #elif (defined __GNUC__)
+// #  define __always_inline             __attribute__((__always_inline__))
+// #elif (defined __ICCARM__)
+// #  define __always_inline             _Pragma("inline=forced")
+// #endif
 
 /**
  * \def __no_inline
